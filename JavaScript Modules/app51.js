30a0cca6a436608e77d37ba192bc72c9015600ae
Kristin Herrmann <dickmann.michael@gmail.com>
2018-09-28 21:52:55 -0600

    for loops

---
+++ JavaScript Modules/app51.js
@@ -0,0 +1,6 @@
+// Write your code below
+//there are 3 parts to a for-loop: an initialization, a stop condition, and an iteration statement.
+
+for (let i = 5; i < 11; i++) {
+    console.log(i)
+  }
