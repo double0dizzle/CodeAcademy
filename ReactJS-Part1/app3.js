a31f47316033f18499011b5a1222f1097b7cf9b2
Kristin Herrmann <dickmann.michael@gmail.com>
2018-10-14 00:08:02 -0600

    Attributes in JSX

---
+++ ReactJS-Part1/app3.js
@@ -0,0 +1,10 @@
+// JSX elements can have attributes, just like HTML elements can.
+
+// A JSX attribute is written using HTML-like syntax: a name, followed by an equals sign, followed by a value. The value should be wrapped in quotes, like this:
+
+// my-attribute-name="my-attribute-value"
+
+
+
+const p1 = <p id="large">foo</p>;
+const p2 = <p id="small">bar</p>
