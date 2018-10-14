56224bb0469ff6d22ea92bbf8195ad50dd3bcb98
Kristin Herrmann <dickmann.michael@gmail.com>
2018-10-14 02:10:53 -0600

    Passing a Variable to ReactDOM.render()

--- ReactJS-Part1/app8.js
+++ ReactJS-Part1/app8.js
@@ -1,3 +1,16 @@
+// Move to the right a little more, and you will see this expression:
+
+// document.getElementById('app')
+// You just learned that ReactDOM.render() makes its first argument appear onscreen. But where on the screen should that first argument appear?
+
+// The first argument is appended to whatever element is selected by the second argument.
+
+// In the code editor, select index.html. See if you can find an element that would be selected by document.getElementById('app').
+
+// That element acted as a container for ReactDOM.render()'s first argument!
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
