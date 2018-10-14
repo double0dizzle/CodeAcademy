e0976cd306d0f29ac8931db3d422f5cb5129e5d0
Kristin Herrmann <dickmann.michael@gmail.com>
2018-10-14 00:03:30 -0600

    JSX Elements And Their Surroundings

---
+++ ReactJS-Part1/app2.js
@@ -0,0 +1,5 @@
+// JSX elements are treated as JavaScript expressions. They can go anywhere that JavaScript expressions can go.
+
+// That means that a JSX element can be saved in a variable, passed to a function, stored in an object or array...you name it.
+
+const myArticle = <article></article>;
