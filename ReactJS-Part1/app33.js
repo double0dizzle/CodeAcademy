97d3d9587758ac6c9a9ebeedcaed3e48963fd863
Kristin Herrmann <dickmann.michael@gmail.com>
2018-10-14 23:14:02 -0600

    Use A Variable Attribute in a Component

---
+++ ReactJS-Part1/app33.js
@@ -0,0 +1,40 @@
+// Use a Variable Attribute in a Component
+// Take a look at this JavaScript object named redPanda:
+
+// const redPanda = {
+//   src:  'https://upload.wikimedia.org/wikipedia/commons/b/b2/Endangered_Red_Panda.jpg',
+//   alt: 'Red Panda',
+//   width:  '200px
+// };
+// How could you render a React component, and get a picture with redPanda's properties?
+
+// Select RedPanda.js to see one way to do it.
+
+// Note all of the curly-brace JavaScript injections inside of the render function! Lines 16, 17, and 18 all use JavaScript injections.
+
+// You can, and often will, inject JavaScript into JSX inside of a render function.
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const owl = {
+  title: 'Excellent Owl',
+  src: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-owl.jpg'
+};
+
+// Component class starts here:
+class Owl extends React.Component {
+  render() {
+    return  (
+      <div>
+      <h1>{owl.title}</h1>
+        <img src={owl.src} alt={owl.title}/>
+    </div>
+    );
+  }
+};
+
+ReactDOM.render(<Owl />, document.getElementById('app'));
