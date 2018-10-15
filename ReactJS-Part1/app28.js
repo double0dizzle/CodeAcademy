f1b5d48a54cb11c96fcab3569e8869d1e8fbf07c
Kristin Herrmann <dickmann.michael@gmail.com>
2018-10-14 21:43:18 -0600

    name a component class

---
+++ ReactJS-Part1/app28.js
@@ -0,0 +1,15 @@
+// Name a Component Class
+// Good! Subclassing React.Component is the way to declare a new component class.
+
+// When you declare a new component class, you need to give that component class a name. On line 4, notice that our component class's name is MyComponentClass.
+
+// Component class variable names must begin with capital letters!
+
+// This adheres to JavaScript's class syntax. It also adheres to a broader programming convention in which class names are written in UpperCamelCase.
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MyComponentClass extends React.Component{}
