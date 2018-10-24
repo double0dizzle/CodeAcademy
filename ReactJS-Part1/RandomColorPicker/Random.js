0fc7fc88f23bea568757d921e5d240e6a5616232
Kristin Herrmann <dickmann.michael@gmail.com>
2018-10-24 17:22:06 -0600

    Random Color Picker

---
+++ ReactJS-Part1/RandomColorPicker/Random.js
@@ -0,0 +1,66 @@
+// Random Color Picker
+// In this project, we'll build a program that helps designers think of new color schemes.
+
+// Our program will set the screen's background to a random color. Clicking a button will refresh to a new, random color. Random generators are a well-known tool for breaking a creative rut.
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from './Button';
+
+class Random extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { color: [161, 241, 85] };
+    this.handleClick = this.handleClick.bind(this)
+  }
+  componentDidMount() {
+    this.applyColor();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.applyColor();
+  }
+
+  formatColor(ary) {
+    return 'rgb(' + ary.join(', ') + ')';
+  }
+
+  isLight() {
+    const rgb = this.state.color;
+    return rgb.reduce((a,b) => a+b) < 127 * 3;
+  }
+
+  applyColor() {
+    const color = this.formatColor(this.state.color);
+    document.body.style.background = color;
+  }
+
+  chooseColor() {
+    const random = [];
+    for (let i = 0; i < 3; i++) {
+      random.push(Math.floor(Math.random()*256));
+    }
+    return random;
+  }
+  handleClick(){
+    this.setState({
+      color: this.chooseColor()
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className={this.isLight() ? 'white' : 'black'}>Your color is {this.formatColor(this.state.color)}.
+  
+        </h1>
+        <Button onClick={this.handleClick} light={this.isLight()}/>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Random />, 
+  document.getElementById('app')
+);
