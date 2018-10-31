17b74b4c1fc790e01bb05644014ad0285f3df39c
Kristin Herrmann <dickmann.michael@gmail.com>
2018-10-31 02:33:09 -0600

    children

--- JQuery-Modules/app23.js
+++ JQuery-Modules/app23.js
@@ -21,3 +21,17 @@ $(document).ready(() => {
       $('.menu-button').toggleClass('button-active')
     })  
   }); 
+
+
+//   Review: Style Methods
+// jQuery can be a wizard at managing your CSS, but as with any powerful magic, it must be handled with care and consideration or you'll end up with a giant mess!
+
+// There may be instances where modifying specific CSS properties with jQuery makes sense, but as a best practice, it's best to maintain an organized CSS file and use jQuery to manipulate clearly defined and well-named CSS classes.
+
+// In this lesson, you learned:
+
+// The .css() method can change style properties of an element.
+// The .css() method can accept multiple styles at once if you pass it a JavaScript object as its argument.
+// The .animate() method can change specific style properties over a period of time.
+// The .addClass() will add a CSS class to an element, and the .removeClass() method will remove a CSS class.
+// The .toggleClass() method will toggle a class on or off an element.
