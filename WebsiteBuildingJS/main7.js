4202c38ac5971efa86c15df894fd9c79e8db2c99
Kristin Herrmann <dickmann.michael@gmail.com>
2019-01-19 21:49:52 -0700

    What is an event

--- WebsiteBuildingJS/main7.js
+++ WebsiteBuildingJS/main7.js
@@ -23,3 +23,16 @@
 let first = document.body.firstChild;
 first.innerHTML = "First child";
 first.parentNode.innerHTML = "I am the parent and my inner HTML has been replaced!";
+
+
+// Review
+// In this lesson, you manipulated a webpage structure by leveraging the Document Object Model interface in JavaScript.
+
+// Let's review what we learned:
+
+// The document keyword grants access to the root of the DOM in JavaScript
+// The DOM Interface allows you to select a specific element with CSS selectors by using the .querySelector() method
+// You can also access an element directly by its ID with .getElementById()
+// The .innerHTML and .style properties allow you to modify an element by changing its contents or style respectively
+// You can create, append, and remove elements by using the .createElement(),.appendChild() and .removeChild() methods respectively
+// The .onclick property can add interactivity to a DOM element based on a click event
---
+++ WebsiteBuildingJS/main8.js
@@ -0,0 +1,17 @@
+// What is an Event?
+// When you refresh your email inbox, double tap on a post, or scroll through a newsfeed — something cool happens in your browser. These actions are known as events!
+
+// Events on the web are user interactions and browser manipulations that you can program to trigger functionality. Some examples of events are:
+
+// A mouse clicking on a button
+// Webpage files loading in the browser
+// A user swiping right on an image
+// You can create interactivity on a website by assigning a function to respond to a specific event firing, or triggering. In this lesson, you'll learn to use JavaScript with events to create dynamic websites.
+
+// Instructions
+// Check out the recipe webpage — you can play around with the events and their specific functions!
+
+// Increase the measurements of the ingredients by clicking the + button.
+// Hover over the instructions to see the estimated cook time.
+// Click on highlighted ingredients in the instructions for facts about the foods in the ingredient list.
+// Click the bar at the bottom to change its background color.
