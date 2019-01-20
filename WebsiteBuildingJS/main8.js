3cd4588f9c86c67d8ad7d10ea2010452797ace7e
Kristin Herrmann <dickmann.michael@gmail.com>
2019-01-19 21:52:10 -0700

    Firing events

--- WebsiteBuildingJS/main8.js
+++ WebsiteBuildingJS/main8.js
@@ -15,3 +15,12 @@
 // Hover over the instructions to see the estimated cook time.
 // Click on highlighted ingredients in the instructions for facts about the foods in the ingredient list.
 // Click the bar at the bottom to change its background color.
+
+// "Firing" Events
+// After a specific event fires on a specific element in the document object model (or DOM), an event handler function can be created to run as a response. In this lesson, we will learn about event handler functions that modify and update DOM elements after an event fires.
+
+// Let's compare this to something more familiar: a cuckoo clock is an example of programming with events! When the clock hands strike a new hour, the cuckoo bird responds with a whistle for each hour. For example, the cuckoo bird will whistle twice when the clock strikes 2 o'clock.
+
+// As you can see in the diagram, the clock striking an hour is the specific event that causes a specific response from the cuckoo bird. Event handler functions wait for their specific events to fire like the cuckoo bird in the clock awaiting the next hour. These functions can be used to change a DOM element's color, text and much more!
+
+// Most events in the browser take place without being noticed — the events are undetected because there is no event handler associated with the event to execute an action. Event handlers are crucial for creating interactive websites with JavaScript.
