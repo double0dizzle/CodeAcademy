9b17e33578db085b65e5bb93d394888a3e723b40
Kristin Herrmann <dickmann.michael@gmail.com>
2019-01-21 17:07:00 -0700

    Implementing Handlebars

---
+++ WebsiteBuildingJS/main17.js
@@ -0,0 +1,11 @@
+// Implementing Handlebars
+// Watch the video to get an in-depth overview of the code used in the previous exercise. In case you want to watch it at a later time, here is the YouTube link.
+
+// The major steps of using Handlebars in a project:
+
+// Add the Handlebars library to your project. — one option is to use a Content Delivery Network (CDN): MDN CDN documentation.
+// Create a Handlebars script in your HTML file.
+// In your JavaScript file, grab the HTML stored in the Handlebars script.
+// Use Handlebars.compile() to return a templating function.
+// Pass in a context object to the templating function to get a compiled template.
+// Render the compiled template onto the web page.
